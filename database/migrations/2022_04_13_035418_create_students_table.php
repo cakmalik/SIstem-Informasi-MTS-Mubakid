a7bb39c0bae8a9a33dc6fd1711bd6248e59fd85a
hasinilmalik <hasinilmalik@gmail.com>
2022-04-21 13:22:21 +0700

    ok

--- database/migrations/2022_04_13_035418_create_students_table.php
+++ database/migrations/2022_04_13_035418_create_students_table.php
@@ -16,6 +16,8 @@ return new class extends Migration
         Schema::create('students', function (Blueprint $table) {
             $table->id();
             $table->bigInteger('user_id')->unsigned();
+            $table->string('grade_id')->nullable();
+            $table->bigInteger('urutan')->nullable();
             $table->string('nis')->nullable();
             $table->string('nama_lengkap');
             $table->string('nisn')->nullable();
